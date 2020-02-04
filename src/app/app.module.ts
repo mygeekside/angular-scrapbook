c9c1a280d297901c57399e3d180dcd90c7bd8e56
mygeekside <varun.jax@gmail.com>
2020-02-04 15:25:14 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { HomepageModule } from './homepage/homepage.module';
 import { AppRoutingModule } from './app.routes';
 import { AppComponent } from './app.component';
 
