93be31dd34d1eb4d201f69e5bb425b36a88b6e15
mygeekside <varun.jax@gmail.com>
2020-02-05 00:05:27 +0530

    1 file modified, 1 added

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FooterComponent } from './footer/footer/footer.component';
-import { NavbarComponent } from './navbar/navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [FooterComponent, NavbarComponent, SearchbarComponent]
 })
 export class ComponentModule { }
