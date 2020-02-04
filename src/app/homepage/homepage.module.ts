b4d1ae9ef8d739ed08ceb7bfda81a98441a64208
mygeekside <varun.jax@gmail.com>
2020-02-04 15:13:23 +0530

    2 files modified, 7 deleted

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './homepage.routes';
-//import { WelcomeComponent } from './welcome/welcome.component';
 
 
 @NgModule({
   imports: [
-    CommonModule,HomeRoutingModule
+    HomeRoutingModule
   ],
   declarations: []
 })
