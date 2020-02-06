42fe8a9812abc4963b47e37d1f478c2ea49eacfc
mygeekside <varun.jax@gmail.com>
2020-02-06 23:36:22 +0530

    2 files modified

--- src/app/stopwatch/stopwatch/stopwatch.component.ts
+++ src/app/stopwatch/stopwatch/stopwatch.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-stopwatch',
-  templateUrl: './stopwatch.component.html',
-  styleUrls: ['./stopwatch.component.css']
+  selector: "app-stopwatch",
+  templateUrl: "./stopwatch.component.html",
+  styleUrls: ["./stopwatch.component.css"]
 })
 export class StopwatchComponent implements OnInit {
+  currtimer = 10;
+  id;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.id = setInterval(() => {
+      if (this.currtimer > 0) this.currtimer--;
+    }, 1000);
   }
 
+  ngOnDestroy() {
+    if (this.id) {
+      clearInterval(this.id);
+    }
+  }
 }
